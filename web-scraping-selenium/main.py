c3eeccd3ab9678d44e2882c01b899f7c49cd0e03
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-03-17 16:41:44 +0100

    Add submit song action (#4)
    * Add action to add song
    * Add headless browser option

--- web-scraping-selenium/main.py
+++ web-scraping-selenium/main.py
@@ -2,17 +2,22 @@ import argparse
 from enum import Enum
 import json
 from pprint import pprint
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.webdriver import WebDriver
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver import EdgeOptions
 
 URL = "https://happy-new-year.javidkhasizada.xyz/"
 
 
 class Action(Enum):
     LIST = "list"
+    ADD = "add"
 
     def __str__(self):
         return self.value
@@ -24,13 +29,28 @@ class Browser(Enum):
     def __str__(self):
         return self.value
 
-def get_driver(browser: Browser) -> WebDriver:
+def get_driver(browser: Browser, headless: bool = True) -> WebDriver:
+
     if browser == Browser.EDGE:
-        return webdriver.Edge()
+        # TODO refactor further
+        if (headless):
+            options = EdgeOptions()
+            options.add_argument("--headless=new")
+            driver = webdriver.Edge(options=options)
+        else:
+            driver = webdriver.Edge()
     elif browser == Browser.CHROME:
-        return webdriver.Chrome()
+        if (headless):
+            options = ChromeOptions()
+            options.add_argument("--headless=new")
+            driver = webdriver.Chrome(options=options)
+        else:
+            driver = webdriver.Chrome()
     else:
         raise ValueError("Invalid browser type")
+    
+    driver.implicitly_wait(2)
+    return driver
 
 
 def get_song_details(song: WebElement) -> dict:
@@ -61,14 +81,60 @@ def get_all_songs() -> dict:
         print(f"An error occurred: {e}")
 
 
+def add_song(song_details: dict) -> None:
+    link = driver.find_element(By.LINK_TEXT, "from here")
+
+    # scroll to the element to be able to click it
+    ActionChains(driver)\
+        .scroll_to_element(link)\
+        .perform()
+    link.click()
+
+    form = driver.find_element(By.ID, "song-form")
+
+    wait = WebDriverWait(driver, timeout=2)
+    wait.until(lambda d : form.is_displayed())
+    
+    name_input = form.find_element(By.ID, "song_name-input")
+    name_input.send_keys(song_details["name"])
+
+    artist_input = form.find_element(By.ID, "song_artist-input")
+    artist_input.send_keys(song_details["author"])
+
+    link_input = form.find_element(By.ID, "song_link-input")
+    link_input.send_keys(song_details["url"])
+
+    submit = driver.find_element(By.XPATH, '//*[@id="song-form"]/button[1]')
+    submit.click()
+
+    output = {
+        "status": "success",
+        "message": "Song added successfully"
+    }
+    return output
+
+
 def main(driver: WebDriver, action: Enum, output_file: str) -> None:
     driver.get(URL)
     
     if action == Action.LIST:
         output = get_all_songs()
+    elif action == Action.ADD:
+        song_details = {
+            "name": "Space Song",
+            "author": "Beach House",
+            "url": "https://music.youtube.com/watch?v=uSDWUx7S8dw",
+        }
+        output = add_song(song_details=song_details)
+        
 
-    with open(f"{output_file}.json", "w") as f:
+
+    with open(f"{output_file}.json", "w", encoding="utf-8") as f:
         json.dump(output, f, indent=4)
+
+
+def str_to_bool(s: str) -> bool:
+    return s.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
 
 
 if __name__ == "__main__":
@@ -93,10 +159,16 @@ if __name__ == "__main__":
         type=str,
         default="output",
         help="The output file")
+    args.add_argument(
+        "--debug", "-d",
+        type=str_to_bool,
+        default=False,
+        help="The output file")
     args = args.parse_args()
 
+
     try:
-        driver = get_driver(args.browser)
+        driver = get_driver(args.browser, headless=not args.debug)
     except ValueError as e:
         print(f"An error occurred: {e}")
         exit(1)
